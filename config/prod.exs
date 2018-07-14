0d81b31570f86f794ebad11101566732202ed663
Bartosz Kalinowski <kelostrada@gmail.com>
2018-07-14 12:00:58 +0200

    Change domain to fear-game.herokuapp.com

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :fear, FearWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "gentle-escarpment-87465.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "fear-game.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
